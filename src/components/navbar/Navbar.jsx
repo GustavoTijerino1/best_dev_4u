e9fe44a01f8f6cc3e89236f2fdf848d6de630710
Gustavo Tijerino <busg26@gmail.com>
2022-06-11 15:42:58 -0500

    Updated Navbar and homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,84 +1,110 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
-import { MenuIcon,XIcon } from '@heroicons/react/outline'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { FaReact } from "react-icons/fa";
 
-
-
 export default function Navbar() {
-    const [menu, setMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
 
-    // const handleOpenNavMenu = () => {
-    //     setMenu(!true)
-    // }
+  // const handleOpenNavMenu = () => {
+  //     setMenu(!true)
+  // }
 
-    return (
-        <>
-        <header className='bg-gradient-to-r from-cyan-500 to-blue-500 
+  return (
+    // <>
+    <header
+      className="bg-gradient-to-br from-pink-500 to-orange-400  fixed top-0 left-0 right-0
     
         flex justify-between p-3 text-white
         sm:bg-white
-        '>
-            <div className='flex space-x-2 '>
+        "
+    >
+      <div className="flex space-x-2 ">
+        <FaReact className="h-10 w-10 animate-spin hover:animate-ping" />
+        <a href="/">
+          <h1 className="text-3xl font-bold underline  font-mono   hidden md:block">
+            Gustavo Tijerino
+          </h1>
+        </a>
+      </div>
 
-             <FaReact className="h-10 w-10 animate-spin"/>
-
-            <h1 className="text-3xl font-bold underline  font-mono   hidden md:block">
-                Gustavo Tijerino
-            </h1>
-            </div>
-            <img className="h-10 w-10 rounded-full md:hidden " src="{person.imageUrl}" alt="" />
-
-            <div className='flex justify-center '>
-               
-{/*                 
+      <div className="flex justify-center ">
+        {/*                 
                 <img class="h-10 w-10 rounded-full hidden md:block" src="{person.imageUrl}" alt="" /> */}
+        <a href="/">
+          <h1 className="text-3xl  font-mono text-center  delay-75  md:hidden">
+            G.T
+          </h1>
+        </a>
+      </div>
+      <div className="md:hidden relative">
+        <button className="" onClick={() => setMenu(!menu)}>
+          {menu && <XIcon className="h-6 w-6  animate-pulse mt-1" />}
+          {!menu && <MenuIcon className="h-6 w-6 mt-1" />}
+        </button>
 
-                <h1 className="text-3xl  font-mono text-center  delay-75  md:hidden">
-                    G.T
-                </h1>
-
-            </div>
-            <div className='md:hidden relative' >
-                <button  className='' onClick={() => setMenu(!menu)}>
-                {menu && (<XIcon className="h-6 w-6  animate-pulse mt-1" />)}
-                          {!menu && (
-                           <MenuIcon className="h-6 w-6 mt-1" />)}
-                    
-                </button>
-
-                            {menu ?
-                                <div className='
+        {menu ? (
+          <nav
+            className="
                                 absolute 
                                 right-0
                                 mt-2
                                 bg-white 
-                                rounded-lg py-2 shadow-2xl'>
-            
-                                    <Link to='/' className="block px-4 py-2 text-gray-800
-                                    hover:bg-blue-500" >Homepage</Link>
-                                    <Link to='/' className="block 
-                                    px-4 py-2 text-gray-800 hover:bg-blue-500">Bio</Link>
-                                    <Link to='/' className="block px-4 py-2 text-gray-800 hover:bg-blue-500">
-                                        Projects</Link>
-            
-                                    <a  className='block px-4 py-2 text-gray-800 hover:bg-blue-500' href="/">Resume</a>
-            
-                                </div> : null}
-            </div>
+                                rounded-lg py-2 shadow-2xl"
+          >
+            <Link
+              to="/"
+              className="block px-4 py-2 text-gray-800
+                                    hover:bg-blue-500"
+            >
+              Homepage
+            </Link>
+            <Link
+              to="/"
+              className="block 
+                                    px-4 py-2 text-gray-800 hover:bg-blue-500"
+            >
+              Bio
+            </Link>
+            <Link
+              to="/"
+              className="block px-4 py-2 text-gray-800 hover:bg-blue-500"
+            >
+              Projects
+            </Link>
 
+            <a
+              className="block px-4 py-2 text-gray-800 hover:bg-blue-500"
+              rel="noreferrer"
+              href="  https://docs.google.com/document/d/1tLv9REAxBM-w8b7GNel2XYWGM7Pbz-Z2jMlXjFFQlys/export?format=pdf"
+              download
+            >
+              Resume
+            </a>
+          </nav>
+        ) : null}
+      </div>
 
-
-            <div className='space-x-10 p-1 mr-5  hidden md:block'>
-
-                <Link to='/' className=" hover:underline" >Homepage</Link>
-                <Link to='/' className=" hover:underline" >Bio</Link>
-                <Link to='/' className=" hover:underline" >Projects</Link>
-                <a href="/">Resume</a>
-            </div>
-
-        </header >
-            
-                                    </>
-    )
+      <nav className="space-x-10 p-1 mr-5  hidden md:block">
+        <Link to="/" className=" hover:underline">
+          Homepage
+        </Link>
+        <Link to="/" className=" hover:underline">
+          Bio
+        </Link>
+        <Link to="/" className=" hover:underline">
+          Projects
+        </Link>
+        <a
+          className="hover:underline"
+          rel="noreferrer"
+          href="  https://docs.google.com/document/d/1tLv9REAxBM-w8b7GNel2XYWGM7Pbz-Z2jMlXjFFQlys/export?format=pdf"
+          download
+        >
+          Resume
+        </a>
+      </nav>
+    </header>
+    // </>
+  );
 }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,52 +1,81 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import { FaGithub,FaLinkedin,FaEnvelopeSquare } from "react-icons/fa";
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import { FaGithub, FaLinkedin, FaEnvelopeSquare } from "react-icons/fa";
 
 export default function Homepage() {
-    return (
-        <>
-            <Navbar></Navbar>
-            <aside className="w-64 mt-20  "  >
-                {/* <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded border-4 border-indigo-500/100"> */}
+  return (
+    <>
+      <Navbar></Navbar>
 
+      <aside className="w-64 mt-20 ">
+        {/* <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded border-4 border-indigo-500/100"> */}
 
-                    <ul className='space-y-20  mt-10 fixed'>
-
-                        <li>
-                            <a href="https://github.com/GustavoTijerino1" target="_blank" rel="noopener noreferrer">
-
-                            <FaGithub className="
+        <ul className="space-y-20  mt-20 fixed">
+          <li className="mt-8">
+            <a
+              href="https://github.com/GustavoTijerino1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub
+                className="
                             rounded-full
-                            h-8 w-8 mt-1  hover:animate-ping bg-white" />
-                            </a>
-                            </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/gustavo-tijerino-203647224/" target="_blank" rel="noopener noreferrer">
-
-                            <FaLinkedin className="
+                            h-8 w-8 mt-1  hover:animate-ping bg-white"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/gustavo-tijerino-203647224/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin
+                style={{ color: "blue" }}
+                className="
                               rounded
                             bg-white
                             h-8 w-8 
                             mt-1
-                            hover:animate-spin" />
-                            </a>
-                            </li>
-                        <li>
-                            <a href="http://google.com" target="_blank" rel="noopener noreferrer">
-
-                            <FaEnvelopeSquare className="
+                            hover:animate-spin"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaEnvelopeSquare
+                className="
                             bg-white
                             rounded
                             h-8 w-8 mt-1
-                            hover:motion-safe:animate-bounce" />
-                            </a>
-                            </li>
-                    </ul>
-              
-                {/* </div> */}
-                
-            </aside>
+                            hover:motion-safe:animate-bounce"
+              />
+            </a>
+          </li>
+        </ul>
 
-        </>
-    )
+        {/* </div> */}
+      </aside>
+
+      <div className="flex justify-center p-20 ">
+        <div
+          className="border-2 border-rose-500
+         bg-gradient-to-r from-purple-500 to-pink-500 
+        rounded w-fit mt-18 p-8 text-center text-sm"
+        >
+          <h3>Welcome!!</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            tempore, eos adipisci, neque, quibusdam nisi fugiat ratione
+            praesentium provident autem illum est! Totam sequi repellat a
+            sapiente expedita reiciendis modi?
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
