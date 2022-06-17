aa302ac53f67b17fb273eb0848ec7b9abf28501d
Gustavo Tijerino <busg26@gmail.com>
2022-06-17 00:16:51 -0500

    Updated Resume page and fixed contact link

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -37,7 +37,76 @@ export default function Contact() {
   return (
     <>
       <Navbar></Navbar>
-      <h2>Hello World</h2>
+      <div
+        className="flex justify-center mt-20 p-20 
+      
+     border-2 border-rose-500"
+      >
+        <div
+          className="flex 
+        
+        md:justify-end border-2 border-rose-500 mt-10 p-20 w-fit justify-center"
+        >
+          <form
+            className="flex-wrap 
+            space-y-2 flex-col"
+          >
+            {/* {showAlert && ( */}
+            <div className="">
+              <h1>Email me!!</h1>
+              <p className="">
+                Let us work together and build out of this world projects :D
+              </p>
+            </div>
+            <div className="">
+              <input
+                className="rounded p-2"
+                placeholder="Name"
+                type="text"
+                name="name"
+                required
+                //   onChange={handleInputChange}
+              />
+            </div>
+
+            <div>
+              <input
+                className="rounded p-2"
+                name="email"
+                type="email"
+                placeholder="Email"
+                //   onChange={handleInputChange}
+                //   value={userFormData.email}
+                required
+              />
+            </div>
+
+            <textarea
+              className="resize-none md:resize
+              rounded p-2"
+              placeholder="Message..."
+              name="message"
+              required
+
+              // onChange={handleInputChange}
+            />
+
+            <button
+              type="submit"
+              // disabled={
+              //   !(userFormData.name && userFormData.email && userFormData.message)
+              // }
+            >
+              Submit
+            </button>
+          </form>
+          {/* <Link to="/">
+            <div className="span-border">
+              <span className="span-link">Back To Home</span>
+            </div>
+          </Link> */}
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidecontactbar from "../../components/sidecontactbar/Sidecontactbar";
+import { Link } from "react-router-dom";
 
 export default function Resume() {
-  // const resume = require("../../assets/img/G.M.T.png");
-
   return (
     <div>
       <Navbar />
       <Sidecontactbar />
-      <div className="fixed border-2 border-green-500 bottom-60 right-0 mr-2  ">
-        <h1>Lets Talk!</h1>
-      </div>
+      <Link to="/contact">
+        <button className="fixed border-2 p-2 rounded-full hover:bg-blue-600 hover:underline bottom-60 right-0 mr-2   ">
+          Lets Talk!
+        </button>
+      </Link>
       <div className="border-2 border-red-500 flex justify-center  flex-col space-y-2 items-center p-4">
-        <div className="w-fit border-2 border-blue-500 p-10 text-justify">
+        <div className="border-2 p-2 rounded-full">
           <a
             className="hover:underline"
             rel="noreferrer"
             href="  https://docs.google.com/document/d/1tLv9REAxBM-w8b7GNel2XYWGM7Pbz-Z2jMlXjFFQlys/export?format=pdf"
             download
           >
-            <button>Download Resume</button>
+            <button className="">Download Resume</button>
           </a>
         </div>
         <div className=" border-2 border-blue-500 p-10 text-justify">
@@ -30,16 +31,20 @@ export default function Resume() {
             alt=""
           />
         </div>
-        <div className="w-fit border-2 border-blue-500 p-10 text-justify">
+
+        <button
+          className="hover:animate-pulse 
+          border-2 p-2 rounded-full hover:bg-blue-600 hover:underline"
+        >
           <a
-            className="hover:underline"
+            className=""
             rel="noreferrer"
             href="  https://docs.google.com/document/d/1tLv9REAxBM-w8b7GNel2XYWGM7Pbz-Z2jMlXjFFQlys/export?format=pdf"
             download
           >
-            <button>Download Resume</button>
+            Download Resume
           </a>
-        </div>
+        </button>
       </div>
     </div>
   );
